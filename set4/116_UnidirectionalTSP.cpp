94247dcb9ea2f5bd92b2c0de92be011752029b2f
Jeff Chapman II <jac@JAChapmanII.net>
2013-03-04 04:43:41 -0500

    Finish 116; must have 'dun goofed' on a previous submit

--- set4/116_UnidirectionalTSP.cpp
+++ set4/116_UnidirectionalTSP.cpp
@@ -8,14 +8,6 @@
 using namespace std;
 
 int width, height;
-/*
-bool before(vector<int> a, vector<int> b) {
-	for(int i = 0; i < a.size(); ++i)
-		if(a[i] > b[i])
-			return false;
-	return true;
-}
-*/
 int compare(vector<int> a, vector<int> b) {
 	for(int i = 0; i < a.size(); ++i)
 		if(a[i] > b[i])
@@ -25,54 +17,6 @@ int compare(vector<int> a, vector<int> b) {
 	return 0;
 }
 
-
-/*
-struct T {
-	int r;
-	int v;
-	T(int a, int b) : r(a), v(b) { }
-	bool operator<(const T &rhs) const {
-		if(v > rhs.v)
-			return false;
-		if(r < rhs.r)
-			return true;
-		return false;
-	}
-};
-
-vector<int> getPath(map<int, map<int, int> > m, int rs, int cs = width - 2) {
-	vector<int> list;
-
-	int r = rs;
-	list.insert(list.begin(), r + 1);
-	if(cs == -1)
-		return list;
-
-	int c = cs;
-	vector<T> ts;
-	ts.push_back(T(r, m[r][c]));
-	ts.push_back(T(((r - 1 + height) % height), m[((r - 1 + height) % height)][c]));
-	ts.push_back(T(((r + 1 + height) % height), m[((r + 1 + height) % height)][c]));
-	sort(ts.begin(), ts.end());
-
-	vector<int> sub = getPath(m, ts[0].r, cs - 1);
-	if(ts[1].v == ts[0].v) {
-		vector<int> sub2 = getPath(m, ts[1].r, cs - 1);
-		if(before(sub2, sub))
-			sub = sub2;
-	}
-	if(ts[2].v == ts[0].v) {
-		vector<int> sub2 = getPath(m, ts[2].r, cs - 1);
-		if(before(sub2, sub))
-			sub = sub2;
-	}
-
-	sub.push_back(r + 1);
-	//list.insert(list.begin(), sub.begin(), sub.end());
-
-	return sub;
-}
-*/
 
 int main(int argc, char **argv) {
 	cin >> height;
@@ -113,31 +57,27 @@ int main(int argc, char **argv) {
 
 				m[i][j] = m[a][jm1];
 				vector<int> path = p[a][jm1];
-				//int toAdd = a;
 
 				if(m[b][jm1] < m[i][j]) {
 					m[i][j] = m[b][jm1];
-					//toAdd = b;
 					path = p[b][jm1];
 				} else if(m[b][jm1] == m[i][j]) {
 					vector<int> tmp = p[b][jm1];
-					//int cmp = compare(tmp, path);
-					if(before(tmp, path)) {
+					int cmp = compare(tmp, path);
+					if(cmp < 0) {
 						m[i][j] = m[b][jm1];
-						//toAdd = b;
 						path = tmp;
 					}
 				}
 
 				if(m[c][jm1] < m[i][j]) {
 					m[i][j] = m[c][jm1];
-					//toAdd = c;
 					path = p[c][jm1];
 				} else if(m[c][jm1] == m[i][j]) {
 					vector<int> tmp = p[c][jm1];
-					if(before(tmp, path)) {
+					int cmp = compare(tmp, path);
+					if(cmp < 0) {
 						m[i][j] = m[c][jm1];
-						//toAdd = c;
 						path = tmp;
 					}
 				}
@@ -167,28 +107,14 @@ int main(int argc, char **argv) {
 				l = p[i][width - 1];
 			} else if(m[i][width - 1] == minn) {
 				vector<int> tmp = p[i][width - 1];
-				if(before(tmp, l)) {
+				int cmp = compare(tmp, l);
+				if(cmp < 0) {
 					minI = i;
 					minn = m[i][width - 1];
 					l = p[i][width - 1];
 				}
 			}
-			/*
-			if(m[i][width - 1] < minn) {
-				minI = i;
-				minn = m[i][width - 1];
-				l = getPath(m, i);
-			} else if(m[i][width - 1] == minn) {
-				vector<int> tmp = getPath(m, i);
-				if(before(tmp, l)) {
-					minI = i;
-					minn = m[i][width - 1];
-					l = tmp;
-				}
-			}
-			*/
 		}
-		//cout << "minI: " << minI << endl;
 
 		stringstream ss;
 		for(int i = 0; i < l.size(); ++i)
